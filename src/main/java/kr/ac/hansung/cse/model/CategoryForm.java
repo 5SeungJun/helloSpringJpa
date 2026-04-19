b634c1200dc24afba78ad7bda2c259fbabe91cd5
ohsj <dhtmdwns319@naver.com>
2026-04-19 21:14:31 +0900

    feat : 카테고리 등록 폼 DTO

---
+++ src/main/java/kr/ac/hansung/cse/model/CategoryForm.java
@@ -0,0 +1,15 @@
+package kr.ac.hansung.cse.model;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CategoryForm {
+    @NotBlank(message = "카테고리 이름을 입력하세요")
+    @Size(max = 50, message = "50자 이내로 입력하세요")
+    private String name;
+}
